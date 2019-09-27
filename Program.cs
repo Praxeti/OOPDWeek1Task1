33d3c5d87f2440bc8dfcd0489341ea6e9aa84072
Praxeti <cowsruleman@gmail.com>
2019-09-27 15:24:51 +0000

    Added more variables + write/readlines

--- Program.cs
+++ Program.cs
@@ -9,11 +9,38 @@ namespace OOPDWeek1Task1
         static void Main(string[] args)
         {
             string quest1;
+            string quest2;
+            string quest3;
+            string quest4;
+            string quest5;
+            string quest6;
+            string quest7;
+            string quest8;
+            string quest9;
+            string quest10;
 
             Console.WriteLine("Welcome to the ice cream selection program!");
             Console.WriteLine("You will be asked some questions that will provide you with which ice cream flavour you desire");
-            Console.WriteLine("Which flavour do you like most, out of R, G and B?");
+            Console.WriteLine("Person 1, Which flavour do you like most, out of R, G and B?");
             quest1 = Console.ReadLine();
+            Console.WriteLine("Person 2, Which flavour do you like most, out of R, G and B?");
+            quest2 = Console.ReadLine();
+            Console.WriteLine("Person 3, Which flavour do you like most, out of R, G and B?");
+            quest3 = Console.ReadLine();
+            Console.WriteLine("Person 4, Which flavour do you like most, out of R, G and B?");
+            quest4 = Console.ReadLine();
+            Console.WriteLine("Person 5, Which flavour do you like most, out of R, G and B?");
+            quest5 = Console.ReadLine();
+            Console.WriteLine("Person 6, Which flavour do you like most, out of R, G and B?");
+            quest6 = Console.ReadLine();
+            Console.WriteLine("Person 7, Which flavour do you like most, out of R, G and B?");
+            quest7 = Console.ReadLine();
+            Console.WriteLine("Person 8, Which flavour do you like most, out of R, G and B?");
+            quest8 = Console.ReadLine();
+            Console.WriteLine("Person 9, Which flavour do you like most, out of R, G and B?");
+            quest9 = Console.ReadLine();
+            Console.WriteLine("Person 10, Which flavour do you like most, out of R, G and B?");
+            quest10 = Console.ReadLine();
         }
     }
 }
